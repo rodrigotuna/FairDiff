9f1a7f34441f74ac6139dd737ea0aed422072b9e
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2024-02-19 17:59:32 +0000

    Add DiGress files

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from model.fairdiff import FairDiff
+
+def main():
+    model = FairDiff()
+    print("Done")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/model/fairdiff.py
@@ -0,0 +1,13 @@
+from model.digress.diffusion_model_discrete import DiscreteDenoisingDiffusion
+
+
+class FairDiff:
+    def __init__(self) -> None:
+        self.model = DiscreteDenoisingDiffusion()
+
+    def train(dataset):
+        pass
+
+    def generate():
+        return None
+    
